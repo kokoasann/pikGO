576bbd4e26b7066aaabe1705da7c464fd9ca25e1
kbc18b21 <kbc18b21@stu.kawahara.ac.jp>
2018-08-28 15:58:42 +0900

    unchi

--- Game/Pixie/Pixie.cpp
+++ Game/Pixie/Pixie.cpp
@@ -9,7 +9,7 @@ bool Pixie::Start()
 {
 	pos.Set(500, 0, 0);
 	sr = NewGO < prefab::CSkinModelRender>(0);
-	sr->Init(L"modelData/pixie/pixie.cmo", 0, 0, enFbxUpAxisY);
+	sr->Init(L"modelData/pixie/pixie.cmo");
 	sr->SetPosition(pos);
 	cc.Init(20, 70, pos);
 	return true;
